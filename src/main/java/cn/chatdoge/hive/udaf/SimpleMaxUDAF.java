8e98bee11dccc1ee20ab05dcd6418fd55f9e259c
BigDataMao <mxw712@gmail.com>
2023-08-04 22:24:03 +0800

    打包测试SimpleMaxUDAF

--- src/main/java/cn/chatdoge/hive/udaf/SimpleMaxUDAF.java
+++ src/main/java/cn/chatdoge/hive/udaf/SimpleMaxUDAF.java
@@ -2,14 +2,43 @@ package cn.chatdoge.hive.udaf;
 
 import org.apache.hadoop.hive.ql.exec.UDAF;
 import org.apache.hadoop.hive.ql.exec.UDAFEvaluator;
+import org.apache.hadoop.io.LongWritable;
 
 public class SimpleMaxUDAF extends UDAF {
     public static class a implements UDAFEvaluator{
+        private long max = 0;
         @Override
         public void init() {
             int max = 0;
         }
 
+        public a() {
+            init();
+        }
 
+        public boolean iterate(LongWritable input){
+            if (input == null){
+                return true;
+            }
+            if (input.get() > max){
+                max = input.get();
+            }
+            return true;
+        }
+
+        public long terminatePartial() {
+            return max;
+        }
+
+        public boolean merge(long other){
+            if (other > max){
+                max = other;
+            }
+            return true;
+        }
+
+        public LongWritable terminate(){
+            return new LongWritable(max);
+        }
     }
 }
